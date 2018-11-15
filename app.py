5e14647614a6d0e7074a4a57576369aa4e2e68ae
aboud20-meet <aboud20@meet.mit.edu>
2018-11-15 18:38:06 +0200

    1 out of 3

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
 def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
+    
+@app.route('/cats/<string:name>')
+def Profile(name):
+	return render_template('cat.html', name=name)
 
 if __name__ == '__main__':
    app.run(debug = True)
