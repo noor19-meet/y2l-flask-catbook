3d14eb5d30d9f6aa5846239fd8eee1e9958e17d7
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:39:36 +0200

    a

--- app.py
+++ app.py
@@ -9,10 +9,17 @@ app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
 def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
-    
+    	
 @app.route('/cats/<string:name>')
 def Profile(name):
 	return render_template('cat.html', name=name)
+
+@app.route('/cats' , methods = ['GET', 'POST'])
+def create_cat():
+	if request.method == 'GET':
+		return render_template('create_cat')
+	else:
+		name = request.form['name']
 
 if __name__ == '__main__':
    app.run(debug = True)
